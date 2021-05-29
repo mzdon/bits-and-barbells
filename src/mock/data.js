51160fea290c39296ad79f7815968d6ff888fc42
Marc Zdon <marczdon@gmail.com>
2021-05-29 14:00:49 -0400

    Add a resume and some more stuff

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import resume from '../files/mzdon_resume.pdf';
 
 // HEAD DATA
 export const headData = {
@@ -12,16 +13,18 @@ export const heroData = {
   title: 'Hey, my name is',
   name: 'Marc Zdon',
   subtitle: 'I like to exercise my mind and body',
-  cta: 'Let\'s Go!',
+  cta: "Let's Go!",
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: "That's me and my oldest daughter. My wife is there in the background too!",
+  paragraphTwo:
+    "By trade, I'm a software developer. I love writing code. I'm also really into CrossFit and power lifting. I've got my CrossFit Level 1 coaches certificate... but what CrossFitter doesn't? Honestly. Lift heavy, run fast.",
+  paragraphThree:
+    "I decided it's about time I started recording my thoughts and experiences in both the software and fitness world. Maybe someone will read it? Better yet, maybe someone will benefit from it. That's the hope, at least.",
+  resume,
 };
 
 // PROJECTS DATA
@@ -29,11 +32,10 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Bits & Barbells',
+    info: "Well, I put this site together. It wasn't a huge lift.. just a quick React app put together using Gatsby and a hacked up starter template!",
+    url: 'https://bitsandbarbells.com',
+    repo: 'https://github.com/mzdon/bits-and-barbells',
   },
   {
     id: nanoid(),
